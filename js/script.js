1c0e4baedd3783ed7c6194b44e0b2dceb7ee9887
Jessie Rong <jessie.rong@ngwt.com>
2014-06-20 13:51:17 +0800

    ticket
    up date new ver

---
+++ js/script.js
@@ -0,0 +1,42 @@
+ // JavaScript Document
+$(function() {
+	//initial
+	fixMenu();
+	tabselect()
+    //mobile support
+ 
+})
+
+/*//window resize
+$(window).resize(function() {
+	fixMenu();
+});*/
+
+/*popwin */
+function popWin(){
+	$("body").append('<div class="shadow"></div>');	
+	$('.popwin').show();
+}
+function closeWin(){
+	$(".shadow").remove();
+	$('.popwin').hide();
+}
+
+function fixMenu(){
+	var winWidth = $(window).width();
+	if ( winWidth >= 320 ){
+		var winHeight = $(window).height();
+		var fixHeight = $(".fix").outerHeight();
+		$(".scroll").height( winHeight - fixHeight );
+	}
+}
+ 
+//tab select
+function tabselect() {
+    $(".menu li").click(function() {
+        $(this).addClass("select").siblings().removeClass("select");
+        var i = $(this).index();
+        $("#detailTab>div").hide().parent().children().eq(i).show();
+    }
+    );
+}
